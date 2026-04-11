02662b541b1f0af8e0e4fd4dc79d704aa5bad27d
MaiconSouzaDevFrontEnd <devfrontend.maiconsouza@gmail.com>
2026-04-11 20:31:45 -0300

    inject

---
+++ restaurant/src/app/componentes/Categorias/index.jsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import estilos from "./Categorias.module.css";
+
+const icone1 = "/entrada.png";
+const icone2 = "/massa.png";
+const icone3 = "/carne.png";
+const icone4 = "/bebidas.png";
+const icone5 = "/salada.png";
+const icone6 = "/sobremesa.png";
+
+export default function Categorias({ handleFiltro, botaoClicado }) {
+  return (
+    <section className={estilos.secao_categorias}>
+      <div className={estilos.container_botoes}>
+        <button className={botaoClicado === "Entradas" ? estilos.acenderBtn : estilos.apagarBtn}
+          onClick={() => handleFiltro("Entradas")}>
+          <Image className={estilos.icone} src={icone1} alt="ícone" />
+          <span>Entradas</span>
+        </button>
+
+        <button className={botaoClicado === "Massas" ? estilos.acenderBtn : estilos.apagarBtn}
+          onClick={() => handleFiltro("Massas")}>
+          <Image className={estilos.icone} src={icone2} alt="ícone" />
+          <span>Massas</span>
+        </button>
+
+        <button className={botaoClicado === "Carnes" ? estilos.acenderBtn : estilos.apagarBtn}
+          onClick={() => handleFiltro("Carnes")}>
+          <Image className={estilos.icone} src={icone3} alt="ícone" />
+          <span>Carnes</span>
+        </button>
+        <button className={botaoClicado === "Bebidas" ? estilos.acenderBtn : estilos.apagarBtn}
+          onClick={() => handleFiltro("Bebidas")}>
+          <Image className={estilos.icone} src={icone4} alt="ícone" />
+          <span>Bebidas</span>
+        </button>
+        <button className={botaoClicado === "Saladas" ? estilos.acenderBtn : estilos.apagarBtn}
+          onClick={() => handleFiltro("Saladas")}>
+          <Image className={estilos.icone} src={icone5} alt="ícone" />
+          <span>Saladas</span>
+        </button>
+        <button className={botaoClicado === "Sobremesas" ? estilos.acenderBtn : estilos.apagarBtn}
+          onClick={() => handleFiltro("Sobremesas")}>
+          <Image className={estilos.icone} src={icone6} alt="ícone" />
+          <span>Sobremesas</span>
+        </button>
+      </div>
+    </section>
+  );
+}
--- restaurant/src/app/page.js
+++ restaurant/src/app/page.js
@@ -1,48 +1,118 @@
-import Image from "next/image";
+/* import "./globals.css";
 import styles from "./page.module.css";
+import Image from "next/image";
+
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <main className={styles.page}>
 
-      <main className={styles.main}>
+      <header className={styles.header}>
 
-        <header className={styles.header}>
+        <section className={styles.logo}>
 
-          <section className={styles.logo}>
+          <h1 className={styles.title-logo}>RESTAURANT</h1>
 
-            <h1 className={styles.title-logo}>RESTAURANT</h1>
+          <p className={styles.subtitle-logo}>De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados</p> 
+        
+        </section>
+      </header>
 
-            <p className={styles.subtitle-logo}>De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados</p> 
-          
-          </section>
-        </header>
+      <main className={styles.mainContent}>
+        
+        <section className={styles.categorias}>
 
-        <main className={styles.mainContent}>
-          
-          <section className={styles.categorias}>
+          <button className={styles.buttonCategorias}>Entradas</button>
+          <button className={styles.buttonCategorias}>Massas</button>
+          <button className={styles.buttonCategorias}>Carnes</button>
+          <button className={styles.buttonCategorias}>Bebidas</button>
+          <button className={styles.buttonCategorias}>Saladas</button>
+          <button className={styles.buttonCategorias}>Sobremesas</button>
 
-            <button className={styles.buttonCategorias}>Entradas</button>
-            <button className={styles.buttonCategorias}>Massas</button>
-            <button className={styles.buttonCategorias}>Carnes</button>
-            <button className={styles.buttonCategorias}>Bebidas</button>
-            <button className={styles.buttonCategorias}>Saladas</button>
-            <button className={styles.buttonCategorias}>Sobremesas</button>
+        </section>
 
-          </section>
-          <section className={styles.campoDeBusca}>
+        <section className={styles.campoDeBusca}>
+          <figure className={styles.iconBusca}>
+            <Image src="./public/lupa.png" alt="Ícone de busca" width={20} height={20}/>
+          </figure>
 
-          </section>  
-          <section className={styles.sectionCards}>
+          <input className={styles.inputBusca} type="text" placeholder="Buscar..." />
+        </section>  
 
-            <h2 className={styles.titleCards}>Cardápio</h2>
-            <div className={styles.containerCards}>
+        <section className={styles.sectionCards}>
 
-            </div>
-          </section>
-        </main>
+          <h2 className={styles.titleCards}>Cardápio</h2>
+          <div className={styles.containerCards}>
+
+          </div>
+        </section>
       </main>
-    </div>
-
+    </main>
   );
+}
+ */
+'use client';
+import Image from 'next/image';
+import estilos from "./page.module.css";
+import { useState } from "react";
+import {filtrarProdutos,buscarProduto,produtosEntradas,} from "./servico";
+import Cards from './componentes/Cards';
+import Categorias from './componentes/Categorias';
+import CampoDeBusca from './componentes/CampoDeBusca';
+
+const Banner = "/banner.png";
+
+export default function Home() {
+
+  const [dadosFiltrados, setDadosFiltrados] = useState(produtosEntradas);
+  const [textoBuscaDigitado, setTextoBuscaDigitado] = useState("");
+  const [botaoClicado, setBotaoClicado] = useState("Entradas");
+
+  const handleBusca = (textoDigitado) => {
+    setTextoBuscaDigitado(textoDigitado);
+    textoDigitado.length >= 3 && setDadosFiltrados(buscarProduto(textoDigitado));
+    setBotaoClicado("");
+  };
+
+  const handleFiltro = (categoria) => {
+    setTextoBuscaDigitado("");
+    setDadosFiltrados(filtrarProdutos(categoria));
+    setBotaoClicado(categoria);
+  };
+
+  return (
+    <>
+      <header className={estilos.topo}>
+        <Image src={Banner} alt="banner" />
+        <div>
+          <h1>RESTAURANT</h1>
+          <p>
+            De pratos clássicos a criações surpreendentes, nosso cardápio é um
+            requinte de sabores refinados.
+          </p>
+        </div>
+      </header>
+      <main className={estilos.container_principal}>
+        <Categorias
+          handleFiltro={handleFiltro}
+          botaoClicado={botaoClicado}
+        />
+
+        <CampoDeBusca
+          textoBuscaDigitado={textoBuscaDigitado}
+          handleBusca={handleBusca}
+        />
+
+        <section className={estilos.secao_cards}>
+          <h2>Cardápio</h2>
+          <div className={estilos.container_cards}>
+            {dadosFiltrados.map((produto) => (
+              <Cards key={produto.id} produto={produto} />
+            ))}
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
